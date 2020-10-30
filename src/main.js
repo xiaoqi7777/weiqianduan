5c9f77727127d159314f0e8a87ed8f7641836673
xiaoqi7777 <501798525@qq.com>
2020-10-30 23:51:33 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,10 +1,36 @@
 import Vue from 'vue'
 import App from './App.vue'
-import store from './store'
+import router from './router'
+import {registerApplication,start} from 'single-spa'
 
 Vue.config.productionTip = false
 
+async function loadScript(url){
+  return new Promise((res,rej)=>{
+    let script = document.createElement('script')
+    script.src = url;
+    script.onload = res;
+    script.onerror = rej;
+    document.head.appendChild(script)
+  })
+}
+
+//  singleSpa 缺陷 不够灵活 不能动态加载js文件
+//  样式不隔离 没有js沙箱的机制
+
+registerApplication('myuepp',async ()=>{
+    console.log('加载模块')
+    await loadScript('http://localhost:10001/js/chunk-vendors.js');
+    await loadScript('http://localhost:10001/js/app.js');
+    return window.singleVue // bootStrap mount unmount
+  },
+  // 用户切换到/vue 的路径下,我需要加载刚才定义的子应用
+  location => location.pathname.startsWith('/vue'),
+  {}
+)
+start();
+
 new Vue({
-  store,
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+  111111111111111111111
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
